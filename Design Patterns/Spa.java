7983ea771e51c694b2dbacaffa8358eed025417a
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2017-12-09 18:57:37 -0800

    Update Spa.java

--- Design Patterns/Spa.java
+++ Design Patterns/Spa.java
@@ -2,5 +2,19 @@ package project.designpattern;
 
 public class Spa {
 
-	public static final int cost = 20;
+	public static final int COST = 20;
+	
+	protected Amenity room;
+	
+	 public Spa(Amenity room) {
+		  this.room = room;
+		 }
+		 
+		 public String getDescription() {
+		  return room.getDescription() + " added food bar.";
+		 }
+
+		 public double cost() {
+		  return room.cost() + COST;
+		 }
 }
